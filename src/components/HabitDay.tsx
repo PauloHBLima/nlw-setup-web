f4c9a4510b9e4d0c9fddabf4a2b942c10dbae90a
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2023-01-19 01:45:15 -0300

    homepage do projeto

---
+++ src/components/HabitDay.tsx
@@ -0,0 +1,10 @@
+/* interface HabitDayProps {
+    completed: number;
+} */
+
+export function HabitDay(){
+    return (
+        <div className="w-10 h-10 bg-zinc-980 border-2 border-zinc-880 rounded-lg"></div>  
+    )
+}
+ 
