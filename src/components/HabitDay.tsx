004f252bcfecaddefa40fc7423896cf235ba3f4d
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2023-01-22 23:31:49 -0300

    components week, check

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,48 +1,64 @@
-import * as Popover from '@radix-ui/react-popover';
-import clsx from 'clsx';
-import { ProgressBar } from './ProgressBar';
+import * as Popover from "@radix-ui/react-popover";
+import clsx from "clsx";
+import { ProgressBar } from "./ProgressBar";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import { Check } from "phosphor-react";
 
 interface HabitDayProps {
-    completed: number;
-    amount: number
+  completed: number;
+  amount: number;
 }
 
-export function HabitDay({ completed, amount} : HabitDayProps){
-    const completedPorcentege = Math.round((completed / amount) *100)
-    
+export function HabitDay({ completed, amount }: HabitDayProps) {
+  const completedPorcentege = Math.round((completed / amount) * 100);
 
-    return (
-        <Popover.Root>
-        <Popover.Trigger 
+  return (
+    <Popover.Root>
+      <Popover.Trigger
         className={clsx("w-10 h-10 border-2 rounded-lg", {
-            'bg-zinc-900 border-zinc-800': completedPorcentege ===0,
-            'bg-violet-900 border-violet-700': completedPorcentege > 0 && completedPorcentege < 20,
-            'bg-violet-800 border-violet-600': completedPorcentege >= 20 && completedPorcentege < 40,
-            'bg-violet-700 border-violet-500': completedPorcentege >= 40 && completedPorcentege < 60,
-            'bg-violet-600 border-violet-500': completedPorcentege >= 60 && completedPorcentege < 80,
-            'bg-violet-500 border-violet-400': completedPorcentege >= 80,
-        })} 
-        />
-        
+          "bg-zinc-900 border-zinc-800": completedPorcentege === 0,
+          "bg-violet-900 border-violet-700":
+            completedPorcentege > 0 && completedPorcentege < 20,
+          "bg-violet-800 border-violet-600":
+            completedPorcentege >= 20 && completedPorcentege < 40,
+          "bg-violet-700 border-violet-500":
+            completedPorcentege >= 40 && completedPorcentege < 60,
+          "bg-violet-600 border-violet-500":
+            completedPorcentege >= 60 && completedPorcentege < 80,
+          "bg-violet-500 border-violet-400": completedPorcentege >= 80,
+        })}
+      />
 
-        <Popover.Portal>
-            <Popover.Content 
-            className='min-w-[320px] p-6 rounded 2xl bg-zinc-900 flex flex-col'>
-                <span 
-                className='font-semibold text-zinc-400'> 
-                terça-feira
-                </span>
-                <span 
-                className='mt-1 font-extrabold loading-tight text-3xl'> 
-                21/01 
-                </span>
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded 2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">terça-feira</span>
+          <span className="mt-1 font-extrabold loading-tight text-3xl">
+            21/01
+          </span>
 
-                <ProgressBar progress={completedPorcentege} />
-                
-                <Popover.Arrow height={8} width={16} className='fill-zinc-900'/>
-                </Popover.Content>
-        </Popover.Portal>
-        </Popover.Root>
-    )
+          <ProgressBar progress={completedPorcentege} />
+
+          <div className="mt-6 flex flex-col gap-3">
+            <Checkbox.Root 
+            className="flex items-center gap-3 group"
+            >
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 
+              group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+              <Checkbox.Indicator>
+                <Check  size={20} className="text-white"/>
+              </Checkbox.Indicator>
+              </div>
+
+              <span className="font-semibold text-xl text-white leading-tight 
+              group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                Beber 2L de água
+              </span>
+            </Checkbox.Root>
+          </div>
+
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  );
 }
- 
